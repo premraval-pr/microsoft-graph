9b02876348ce8dd15d76c6bc6f630e0577c761de
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-02-24 11:59:21 -0500

    Calendar View Commit

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -7,6 +7,8 @@ import { Event } from 'microsoft-graph';
 import { config } from './Config';
 import { getUserWeekCalendar } from './GraphService';
 import withAuthProvider, { AuthComponentProps } from './AuthProvider';
+import CalendarDayRow from './CalendarDayRow';
+import './Calendar.css';
 
 interface CalendarState {
   eventsLoaded: boolean;
@@ -59,8 +61,64 @@ class Calendar extends React.Component<AuthComponentProps, CalendarState> {
   }
 
   render() {
+    var sunday = moment(this.state.startOfWeek);
+    var monday = moment(sunday).add(1, 'day');
+    var tuesday = moment(monday).add(1, 'day');
+    var wednesday = moment(tuesday).add(1, 'day');
+    var thursday = moment(wednesday).add(1, 'day');
+    var friday = moment(thursday).add(1, 'day');
+    var saturday = moment(friday).add(1, 'day');
+  
     return (
-      <pre><code>{JSON.stringify(this.state.events, null, 2)}</code></pre>
+      <div>
+        <div className="mb-3">
+          <h1 className="mb-3">{sunday.format('MMMM D, YYYY')} - {saturday.format('MMMM D, YYYY')}</h1>
+          <RouterNavLink to="/newevent" className="btn btn-light btn-sm" exact>New event</RouterNavLink>
+        </div>
+        <div className="calendar-week">
+          <div className="table-responsive">
+            <Table size="sm">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Event</th>
+                </tr>
+              </thead>
+              <tbody>
+                <CalendarDayRow
+                  date={sunday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === sunday.day()) } />
+                <CalendarDayRow
+                  date={monday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === monday.day()) } />
+                <CalendarDayRow
+                  date={tuesday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === tuesday.day()) } />
+                <CalendarDayRow
+                  date={wednesday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === wednesday.day()) } />
+                <CalendarDayRow
+                  date={thursday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === thursday.day()) } />
+                <CalendarDayRow
+                  date={friday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === friday.day()) } />
+                <CalendarDayRow
+                  date={saturday}
+                  timeFormat={this.props.user.timeFormat}
+                  events={this.state.events.filter(event => moment(event.start?.dateTime).day() === saturday.day()) } />
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
     );
   }
 }
